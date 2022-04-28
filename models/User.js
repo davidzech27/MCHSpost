70bc58859c4285430fc5fbaef7dd3ec900d4c99b
David Z <davidzech27@gmail.com>
2022-04-27 17:01:00 -0700

    Post and User models

---
+++ models/User.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose"
+
+const UserSchema = new mongoose.Schema({
+	email: {
+		//can be viewed by other users but can't be changed
+		type: String,
+		unique: true
+	},
+	name: {
+		//can be viewed by other users and can be changed by user directly
+		type: String,
+		maxlength: 25
+	},
+	photo: {
+		//can be viewed by other users and can be changed by user directly
+		type: String,
+		default: "https://static.hudl.com/users/prod/222809_f107b4b13eeb4a43bf1c9db8d86791c8.jpg",
+		maxlength: 1000
+	},
+	bio: {
+		//can be viewed by other users and can be changed by user directly
+		type: String,
+		default: "...",
+		maxlength: 10000
+	},
+	posts: {
+		//can be viewed by other users and can be changed by backend
+		type: [mongoose.Types.ObjectId],
+		ref: "Post"
+	},
+	friends: {
+		//can be viewed by other users and can be changed by backend
+		type: [String]
+	},
+	joinedOn: {
+		//can be viewed by other users but can't be changed
+		type: Date,
+		default: Date.now
+	},
+	data: {
+		//can't be viewed by other users but can be changed by backend
+		friendReqs: {
+			sent: [String],
+			recieved: [String]
+		}
+	}
+})
+
+export default mongoose.models.User || mongoose.model("User", UserSchema)
