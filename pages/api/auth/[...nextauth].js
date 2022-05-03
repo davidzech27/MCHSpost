2f7b8979448e8053ec62c179b4a04b00e86a2a1a
David Z <davidzech27@gmail.com>
2022-05-02 22:33:28 -0700

    refactored db connection functions changed next-auth config to use jwt callback so backend only uses getToken

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,46 +2,53 @@ import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 
 import User from "/models/User"
-import useDB from "/lib/db"
+import { connectToDB } from "/lib/db"
+import { getToken } from "next-auth/jwt"
 
-export default NextAuth({
-	providers: [
-		GoogleProvider({
-			clientId: process.env.GOOGLE_CLIENT_ID,
-			clientSecret: process.env.GOOGLE_CLIENT_SECRET
-		})
-	],
-	callbacks: {
-        signIn: useDB(async ({ profile }) => {
-			const newUser = {
-				email: profile.email.slice(0, -15),
-				name: profile.name.slice(0, -8),
-				photo: profile.image
-			}
+const authHandler = (req, res) => {
+    return NextAuth(req, res, {
+        providers: [
+            GoogleProvider({
+                clientId: process.env.GOOGLE_CLIENT_ID,
+                clientSecret: process.env.GOOGLE_CLIENT_SECRET
+            })
+        ],
+        callbacks: {
+            async signin({ profile }) {
+                console.log(profile)
+                if (!profile.email.endsWith("@srcschools.org")) {
+                    return "/signin" //"You must register with an @srcschools.org email address!"
+                }
 
-			if (!profile.email.endsWith("@srcschools.org")) {
-				return "/signin" //"You must register with an @srcschools.org email address!"
+                try {
+                    await connectToDB()
+
+                    const existingUser = await User.findOne({
+                        email: profile.email.slice(0, -15)
+                    }).lean()
+
+                    if (existingUser) {
+                        return true //"Login successful!"
+                    } else {
+                        const newUser = {
+                            email: profile.email.slice(0, -15),
+                            name: profile.name.slice(0, -8),
+                            photo: profile.image
+                        }
+                        await User.create(newUser)
+                        return true //"Account created successfully!"
+                    }
+                } catch (err) {
+                    return "/signin" //"An error has occurred!"
+                }
+            },
+            jwt({ token }) {
+                token.email = token.email.slice(0, -15)
+                delete token.name
+                return token
             }
-			try {
-				let user = await User.findOne({
-					email:  profile.email.slice(0, -15)
-				})
+        }
+    })
+}
 
-                if (user) {
-					return true //"Login successful!"
-				} else {
-					user = await User.create(newUser)
-                    return true //"Account created successfully!"
-				}
-            } catch (err) {
-				return "/signin"
-			}
-        }),
-        session: useDB(async ({ session }) => {
-            const email = session.user.email.slice(0, -15)
-            const user = await User.findOne({ email: email })
-            const newSession = { ...user, expires: session.expires }
-            return newSession
-        })
-	}
-})
+export default authHandler
