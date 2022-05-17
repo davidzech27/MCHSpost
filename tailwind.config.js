4bd2cf62edf4f2e59a7a542422528e4b2f752b9a
David Z <davidzech27@gmail.com>
2022-05-16 22:57:12 -0700

    set up theme in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
         "./components/**/*.jsx"
     ],
     theme: {
-        extend: {}
+        colors: {
+            background: "#051020",
+            surface: "#101C2C",
+            hover: "#374155",
+            text: "#FEFEFF",
+            subtext: "#858AA0",
+            green: "#7EA754",
+            yellow: "#FFD344"
+        },
+        fontFamily: {
+            sans: ["News Cycle", "sans-serif"]
+        },
+        extend: {
+            spacing: {
+                2.5: "0.625rem"
+            }
+        }
     },
     plugins: []
 }
