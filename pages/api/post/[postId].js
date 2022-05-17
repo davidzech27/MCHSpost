be59aa0d7a76dbcb5384a51607b083d770757043
David Z <davidzech27@gmail.com>
2022-05-16 18:57:10 -0700

    send 404 when nonexistant user/post is requested

--- pages/api/post/[postId].js
+++ pages/api/post/[postId].js
@@ -7,22 +7,24 @@ const postHandler = (req, res) => {
     const { postId } = req.query
     const post = await Post.findById(postId).lean()
 
-    if (post.postSetting === "public") {
-        res.status(200).json(post)
-    } else {
-        const { email } = await getToken({ req })
-
-        const posterFriends = await User.findOne({ email: post.poster.email }).select({ friends: 1  }).lean()
-        const posterFriendsEmails = posterFriends.map((posterFriend) => posterFriend.email)
-
-        if (posterFriendsEmails.includes(email)) {
+    if (post) {
+        if (post.postSetting === "public") {
             res.status(200).json(post)
         } else {
-            res.status(403).send("Can't view private post. Not friends with user.")
+            const { email } = await getToken({ req })
+
+            const posterFriends = await User.findOne({ email: post.poster.email }).select({ friends: 1  }).lean()
+            const posterFriendsEmails = posterFriends.map((posterFriend) => posterFriend.email)
+
+            if (posterFriendsEmails.includes(email)) {
+                res.status(200).json(post)
+            } else {
+                res.status(403).send("Can't view private post. Not friends with user.")
+            }
         }
+    } else {
+        res.status(404).send("Post not found.")
     }
-
-
 }
 
 export default withDB(postHandler)
--- pages/api/user/[email].js
+++ pages/api/user/[email].js
@@ -4,7 +4,12 @@ import { withDB } from "/lib/db"
 const userHandler = async (req, res) => {
     const { email } = req.query
     const user = await User.findOne({ email }).select({ data: 0 }).lean()
-    res.status(200).json(user)
+
+    if (user) {
+        res.status(200).json(user)
+    } else {
+        res.status(404).send("User does not exist.")
+    }
 }
 
 export default withDB(userHandler)
