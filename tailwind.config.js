ee3555a94ada527c95b6763142aac38577000a52
David Z <davidzech27@gmail.com>
2022-05-16 23:18:50 -0700

    updated theme to have multiple surface colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,9 @@ module.exports = {
     theme: {
         colors: {
             background: "#051020",
-            surface: "#101C2C",
+            surface1: "#101C2C",
+            surface2: "#192435",
+            surface3: "#242F41",
             hover: "#374155",
             text: "#FEFEFF",
             subtext: "#858AA0",
