49b4e27c8353aceb1f45c0dbf3365f314d382f29
David Z <davidzech27@gmail.com>
2022-05-18 10:08:51 -0700

    fixed handling for unauthenticated user again

--- pages/api/profile/index.js
+++ pages/api/profile/index.js
@@ -3,8 +3,8 @@ import { getToken } from "next-auth/jwt"
 import { withDB } from "/lib/db"
 
 const profileHandler = async (req, res) => {
-    const { email } = await getToken({ req })
-    
+    const email = (await getToken({ req }))?.email
+
     if (email) {
         const profile = await User.findOne({ email }).lean()
         res.status(200).json(profile)
