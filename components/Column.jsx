be8a347d33222a281940b6476ac5644dd3806234
David Z <davidzech27@gmail.com>
2022-05-17 22:09:06 -0700

    column component

---
+++ components/Column.jsx
@@ -0,0 +1,9 @@
+const Column = ({ children }) => {
+    return (
+        <div className="flex flex-col content-start gap-2.5">
+            {children}
+        </div>
+    )
+}
+
+export default Column
