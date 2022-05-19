0be746955688cfc7967c5debb92c3454d7480aee
David Z <davidzech27@gmail.com>
2022-05-18 20:52:55 -0700

    use custom QueryClient config

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,29 @@
 import { QueryClient, QueryClientProvider } from "react-query"
 import { ReactQueryDevtools } from "react-query/devtools"
+import api from "/lib/api"
 
 import Layout from "/components/Layout"
 
 import "/styles/globals.css"
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            queryFn: async ({ queryKey }) => {
+                return (await api.get(queryKey[0])).data
+            },
+            onError: (err) => {
+                console.log(err)
+            },
+            retry: false
+        },
+        mutations: {
+            onError: (err) => {
+                console.log(err.response)
+            }
+        }
+    }
+})
 
 const App = ({ Component, pageProps }) => {
     if (!Component.noLayout) {
