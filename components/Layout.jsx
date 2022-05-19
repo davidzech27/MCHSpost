7229c7d52a80debdd576286aa710034c9c5ccb0a
David Z <davidzech27@gmail.com>
2022-05-19 10:38:41 -0700

    changed some layout css

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -6,16 +6,15 @@ import ExploreBar from "/components/ExploreBar"
 const Layout = ({ children }) => {
     const { profile, unauthenticated } = useProfile()
 
-
     return (
         <div className="min-h-screen flex bg-background">
-            <div className="h-screen relative top-0 basis-1/4 p-8">
+            <div className="h-screen basis-1/4 sticky top-0 p-8">
                 <NavBar authenticatedState={!unauthenticated} />
             </div>
             <div className="min-h-screen basis-1/2 py-8">
                 {children}
             </div>
-            <div className="min-h-screen relative top-0 basis-1/4 p-8">
+            <div className="h-screen basis-1/4 sticky top-0 p-8">
                 <ExploreBar />
             </div>
         </div>
