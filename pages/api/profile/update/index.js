71bd4905d783b95b9285de5568c9e14aa9d2045e
David Z <davidzech27@gmail.com>
2022-05-18 22:11:44 -0700

    made updating profile require post request

--- pages/api/profile/update/index.js
+++ pages/api/profile/update/index.js
@@ -3,13 +3,18 @@ import { getToken } from "next-auth/jwt"
 import { withDB } from "/lib/db"
 
 const updateProfileHandler = (req, res) => {
-    const { email } = await getToken({ req })
+    if (req.method === "POST") {
+        const { email } = await getToken({ req })
 
-    const newFields = {}
-    for (field of ["name", "photo", "bio"]) if (req.body[field]) newFields[field] = req.body[field]
+        const newFields = {}
+        for (field of ["name", "photo", "bio"]) if (req.body[field]) newFields[field] = req.body[field]
 
-    const updatedProfile = await User.findOneAndUpdate({ email }, newFields).lean()
-    res.status(200).json(updatedProfile)
+        const updatedProfile = await User.findOneAndUpdate({ email }, newFields).lean()
+        res.status(200).json(updatedProfile)
+    } else {
+        res.status(405).end()
+    }
+    
 }
 
 export default withDB(updateProfileHandler)
