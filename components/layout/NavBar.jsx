d406c42c760b4e174e773c7413a8947de2a6855c
David Z <davidzech27@gmail.com>
2022-05-19 23:18:17 -0700

    extracted to component NavButtons

--- components/layout/NavBar.jsx
+++ components/layout/NavBar.jsx
@@ -1,22 +1,12 @@
-import Card from "/components/wrapper/Card"
 import Column from "/components/wrapper/Column"
-import Button from "/components/input/Button"
-import LinkButton from "/components/input/LinkButton"
 import AboutPageLink from "/components/layout/AboutPageLink"
+import NavButtons from "/components/layout/NavButtons"
 
 const NavBar = ({ authenticatedState }) => {
     return (
         <Column>
             <AboutPageLink />
-            <Card className="bg-surface1">
-                <Column>
-                    <LinkButton href="/home" className="hover:bg-hover">Home</LinkButton>
-                    {authenticatedState && <LinkButton href="/profile" className="hover:bg-hover">Profile</LinkButton>}
-                    <LinkButton href="/users" className="bg-green hover:opacity-75 text-background">Users</LinkButton>
-                    {authenticatedState && <Button className="bg-yellow hover:opacity-75 text-background">Upload</Button>}
-                    {!authenticatedState && <LinkButton href="/signin" className="bg-yellow hover:opacity-75 text-background">Sign in</LinkButton>}
-                </Column>
-            </Card>
+            <NavButtons authenticatedState={authenticatedState} />
         </Column>
     )
 }
---
+++ components/layout/NavButtons.jsx
@@ -0,0 +1,20 @@
+import Card from "/components/wrapper/Card"
+import Column from "/components/wrapper/Column"
+import Button from "/components/input/Button"
+import LinkButton from "/components/input/LinkButton"
+
+const NavButtons = ({ authenticatedState }) => {
+    return (
+        <Card className="bg-surface1">
+            <Column>
+                <LinkButton href="/home" className="hover:bg-hover">Home</LinkButton>
+                {authenticatedState && <LinkButton href="/profile" className="hover:bg-hover">Profile</LinkButton>}
+                <LinkButton href="/users" className="bg-green hover:opacity-75 text-background">Users</LinkButton>
+                {authenticatedState && <Button className="bg-yellow hover:opacity-75 text-background">Upload</Button>}
+                {!authenticatedState && <LinkButton href="/signin" className="bg-yellow hover:opacity-75 text-background">Sign in</LinkButton>}
+            </Column>
+        </Card>
+    )
+}
+ 
+export default NavButtons
