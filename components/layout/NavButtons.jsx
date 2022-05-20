2b9c35ffec9bd26d00a43fa686580320f4a8f985
David Z <davidzech27@gmail.com>
2022-05-20 10:48:29 -0700

    changed top 2 NavButton colors

--- components/layout/NavButtons.jsx
+++ components/layout/NavButtons.jsx
@@ -7,8 +7,8 @@ const NavButtons = ({ authenticatedState }) => {
     return (
         <Card className="bg-surface1">
             <Column>
-                <LinkButton href="/home" className="hover:bg-hover">Home</LinkButton>
-                {authenticatedState && <LinkButton href="/profile" className="hover:bg-hover">Profile</LinkButton>}
+                <LinkButton href="/home" className="bg-surface2 hover:bg-hover">Home</LinkButton>
+                {authenticatedState && <LinkButton href="/profile" className="bg-surface2 hover:bg-hover">Profile</LinkButton>}
                 <LinkButton href="/users" className="bg-green hover:opacity-75 text-background">Users</LinkButton>
                 {authenticatedState && <Button className="bg-yellow hover:opacity-75 text-background">Upload</Button>}
                 {!authenticatedState && <LinkButton href="/signin" className="bg-yellow hover:opacity-75 text-background">Sign in</LinkButton>}
