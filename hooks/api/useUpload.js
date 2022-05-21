94b8ef082d9501b96b31c2111dcaece4e31f7832
David Z <davidzech27@gmail.com>
2022-05-21 00:46:00 -0700

    made useUpload api hook

---
+++ hooks/api/useUpload.js
@@ -0,0 +1,18 @@
+import { useMutation, useQueryClient } from "react-query"
+import api from "/lib/api"
+
+const useUpload = () => {
+    const queryClient = useQueryClient()
+
+    const { mutate: uploadPost } = useMutation(async (newPost) => {
+        return (await api.post("/post/upload", newPost)).data
+    }, {
+        onSuccess: (newPost) => {
+            queryClient.setQueryData("/feed/public", (publicFeed) => [newPost, ...publicFeed])
+        }
+    })
+
+    return uploadPost
+}
+
+export default useUpload
