632f8e285f5769ef8bb8b845d0f23de2dd09bf0e
David Z <davidzech27@gmail.com>
2022-05-21 18:43:06 -0700

    NavButtons uses UploadModal

--- components/layout/NavButtons.jsx
+++ components/layout/NavButtons.jsx
@@ -1,16 +1,21 @@
+import useModalContext from "/hooks/context/useModalContext"
+
 import Card from "/components/wrapper/Card"
 import Column from "/components/wrapper/Column"
 import Button from "/components/input/Button"
 import LinkButton from "/components/input/LinkButton"
+import UploadModal from "/components/modal/UploadModal"
 
 const NavButtons = ({ authenticatedState }) => {
+    const { setModal } = useModalContext()
+
     return (
         <Card className="bg-surface1">
             <Column>
                 <LinkButton href="/home" className="bg-surface2 hover:bg-hover">Home</LinkButton>
                 {authenticatedState && <LinkButton href="/profile" className="bg-surface2 hover:bg-hover">Profile</LinkButton>}
                 <LinkButton href="/users" className="bg-green hover:opacity-75 text-background">Users</LinkButton>
-                {authenticatedState && <Button className="bg-yellow hover:opacity-75 text-background">Upload</Button>}
+                {authenticatedState && <Button onClick={() => setModal(<UploadModal />)} className="bg-yellow hover:opacity-75 text-background">Upload</Button>}
                 {!authenticatedState && <LinkButton href="/signin" className="bg-yellow hover:opacity-75 text-background">Sign in</LinkButton>}
             </Column>
         </Card>
