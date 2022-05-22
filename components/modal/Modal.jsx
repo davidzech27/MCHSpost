93258dbcd8427d8bd185021b36e80c8ffb7550a7
David Z <davidzech27@gmail.com>
2022-05-21 17:46:01 -0700

    made modal close when clicking outside corners

--- components/modal/Modal.jsx
+++ components/modal/Modal.jsx
@@ -4,7 +4,7 @@ import SmallButton from "/components/input/SmallButton"
 const Modal = ({ children, close }) => {
     return (
         <div onClick={close} className="h-screen w-screen fixed z-10 bg-background bg-opacity-85 flex justify-center items-center">
-            <div onClick={(e) => e.stopPropagation()}>
+            <div onClick={(e) => e.stopPropagation()} className="rounded-3xl">
                 <Card className="w-[32rem] bg-surface1 relative">
                     <SmallButton onClick={close} className="absolute top-3 right-3 bg-surface2 hover:bg-surface3">X</SmallButton>
                     {children}
