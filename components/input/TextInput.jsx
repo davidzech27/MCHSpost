30415ad73b5ccbb3b6b1db47a0b449dc045af6aa
David Z <davidzech27@gmail.com>
2022-05-22 09:56:02 -0700

    made TextInput component

---
+++ components/input/TextInput.jsx
@@ -0,0 +1,7 @@
+const TextInput = ({ value, setValue, placeholder, className }) => {
+    return (
+        <input type="text" value={value} onChange={(e) => setValue(e.target.value)} placeholder={placeholder} className={`w-full pt-3 px-4 rounded-xl text-xl placeholder-subtext focus:placeholder-text transition duration-100 outline-none resize-none ${className}`} />
+    )
+}
+ 
+export default TextInput
