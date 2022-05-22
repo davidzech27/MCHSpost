a2cc32a9f6295aac1c69b9f6e2c1c2df5f7edb7b
David Z <davidzech27@gmail.com>
2022-05-22 09:09:10 -0700

    removed unnecessary onClick prop

--- components/input/Button.jsx
+++ components/input/Button.jsx
@@ -1,6 +1,6 @@
-const Button = ({ children, className, onClick, ...props }) => {
+const Button = ({ children, className, ...props }) => {
     return (
-        <button onClick={onClick} className={`h-20 w-full rounded-2xl text-3xl tracking-wide transition duration-100 ${className}`} {...props}>
+        <button className={`h-20 w-full rounded-2xl text-3xl tracking-wide transition duration-100 ${className}`} {...props}>
             {children}
         </button>
     )
--- components/input/SmallButton.jsx
+++ components/input/SmallButton.jsx
@@ -1,6 +1,6 @@
-const SmallButton = ({ children, className, onClick, ...props }) => {
+const SmallButton = ({ children, className, ...props }) => {
     return (
-        <button onClick={onClick} className={`h-8 min-w-[2rem] rounded-md transition duration-100 ${className}`} {...props}>
+        <button className={`h-8 min-w-[2rem] rounded-md transition duration-100 ${className}`} {...props}>
             {children}
         </button>
     )
