706940055d658e71ef3bce824edfaf0ff3ad5954
David Z <davidzech27@gmail.com>
2022-05-22 00:06:16 -0700

    made functional home page

--- pages/home.jsx
+++ pages/home.jsx
@@ -1,14 +1,35 @@
-import { useState, useEffect } from "react"
+import { useState } from "react"
 import useStickyState from "/hooks/util/useStickyState"
+import useFeed from "/hooks/api/useFeed.js"
+import useUpload from "/hooks/api/useUpload"
+
+import Column from "/components/wrapper/Column"
+import Button from "/components/input/Button"
+import Card from "/components/wrapper/Card"
+import PostCard from "/components/post/PostCard"
+import TextArea from "/components/input/TextArea"
 
 const Home = () => {
     const [feedType, setFeedType] = useStickyState("feedType", "public")
-    
+    const [feed, feedLoaded] = useFeed(feedType)
+
 	return (
-		<>
-            <button onClick={() => setFeedType("public")} disabled={feedType === "public"}>public</button>
-            <button onClick={() => setFeedType("friends")} disabled={feedType === "friends"}>friends</button>
-		</>
+		<Column>
+            <Card className="bg-surface1">
+                <Column>
+                    <Button onClick={() => setFeedType("public")} className="bg-surface2 hover:bg-surface3">Public</Button>
+                    <Button onClick={() => setFeedType("friends")} className="bg-surface2 hover:bg-surface3">Friends</Button>
+                </Column>
+            </Card>
+
+            <Card className="bg-surface1">
+                <Column>
+                    {feedLoaded ?
+                    feed.map((post) => <PostCard post={post} className="bg-surface2 hover:bg-surface3" key={post._id} />) :
+                    "Loading..."}
+                </Column>
+            </Card>
+		</Column>
 	)
 }
 
