298f8584024d7f652883df94bf965f3cb4abf487
David Z <davidzech27@gmail.com>
2022-05-21 23:59:15 -0700

    Modal uses Card's onClick to stopPropagation

--- components/modal/Modal.jsx
+++ components/modal/Modal.jsx
@@ -10,12 +10,10 @@ const Modal = ({ children }) => {
 
     return (
         <div onClick={close} className="h-screen w-screen fixed z-10 bg-background bg-opacity-85 flex justify-center items-center">
-            <div onClick={(e) => e.stopPropagation()} className="rounded-3xl">
-                <Card className="w-[32rem] bg-surface1 relative">
-                    <SmallButton onClick={close} className="absolute top-3 right-3 bg-surface3 hover:bg-hover font-bold">X</SmallButton>
-                    {children}
-                </Card>
-            </div>
+            <Card onClick={(e) => e.stopPropagation()} className="w-[32rem] bg-surface1 relative">
+                <SmallButton onClick={close} className="absolute top-3 right-3 bg-surface3 hover:bg-hover font-bold">X</SmallButton>
+                {children}
+            </Card>
         </div>
     )
 }
