f830ffb9c28939c9da3a55a4a33dfe942a79c015
David Z <davidzech27@gmail.com>
2022-05-24 09:53:44 -0700

    made FeedTypeDropdown component

--- components/layout/FeedTypeDropdown.jsx
+++ components/layout/FeedTypeDropdown.jsx
@@ -5,21 +5,21 @@ const FeedTypeDropdown = ({ feedType, setFeedType }) => {
     const [open, setOpen] = useState(false)
 
     return (
-        <div className="relative">
-            <button onMouseEnter={() => {setOpen(true)}} className={`h-10 w-32 relative z-10 rounded-lg transition duration-100 cursor-default hover:bg-hover ${open ? "bg-surface3" : "bg-surface2"}`}>
-                {feedType === "public" ? "Public feed" : "Friend feed"}
-            </button>
+        <div className="h-10 w-36 relative">
+            <div onMouseLeave={() => setOpen(false)} className="h-[156px] absolute rounded-lg">
+                <button onMouseEnter={() => {setOpen(true)}} className={`h-10 w-36 rounded-lg transition duration-100 cursor-default hover:bg-hover ${open ? "bg-surface3" : "bg-surface2"}`}>
+                    {feedType === "public" ? "Public feed" : "Friend feed"}<svg xmlns="http://www.w3.org/2000/svg" className="h-[22px] w-[22px] inline relative left-1 top-[1px]" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}><path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" /></svg>
+                </button>
 
-            {open && <div onMouseLeave={() => setOpen(false)} className="h-[156px] absolute top-0 rounded-lg flex items-end">
-                <div className="h-28 w-32 p-4 bg-surface1 hover:bg-surface2 rounded-lg shadow-lg shadow-background flex flex-col justify-around transition duration-100">
+                {open && <div className="h-28 w-36 p-4 absolute bottom-0 bg-surface1 hover:bg-surface2 rounded-lg shadow-lg shadow-background flex flex-col justify-around transition duration-100">
                     <SmallButton onClick={() => setFeedType("public")} className="bg-surface3 hover:bg-hover">
                         Public
                     </SmallButton>
                     <SmallButton onClick={() => setFeedType("friends")} className="bg-surface3 hover:bg-hover">
                         Friends
                     </SmallButton>
-                </div>
-            </div>}
+                </div>}
+            </div>
         </div>
     )
 }
