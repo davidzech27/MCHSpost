8d8c5a7624655f796867b5c88dfda75a2d69346e
David Z <davidzech27@gmail.com>
2022-05-24 17:12:07 -0700

    removed w-full from text input components

--- components/input/TextArea.jsx
+++ components/input/TextArea.jsx
@@ -1,6 +1,6 @@
 const TextArea = ({ value, setValue, placeholder, className }) => {
     return (
-        <textarea value={value} onChange={(e) => setValue(e.target.value)} placeholder={placeholder} className={`w-full pt-3 px-4 rounded-xl text-xl placeholder-subtext focus:placeholder-text transition duration-100 outline-none resize-none ${className}`} />
+        <textarea value={value} onChange={(e) => setValue(e.target.value)} placeholder={placeholder} className={`pt-3 px-4 rounded-xl text-xl placeholder-subtext focus:placeholder-text transition duration-100 outline-none resize-none ${className}`} />
     )
 }
  
--- components/input/TextInput.jsx
+++ components/input/TextInput.jsx
@@ -1,6 +1,6 @@
 const TextInput = ({ value, setValue, placeholder, className }) => {
     return (
-        <input type="text" value={value} onChange={(e) => setValue(e.target.value)} placeholder={placeholder} className={`h-fit w-full px-4 py-3 rounded-xl text-xl placeholder-subtext focus:placeholder-text transition duration-100 outline-none resize-none ${className}`} />
+        <input type="text" value={value} onChange={(e) => setValue(e.target.value)} placeholder={placeholder} className={`h-fit px-4 py-3 rounded-xl text-xl placeholder-subtext focus:placeholder-text transition duration-100 outline-none resize-none ${className}`} />
     )
 }
  
