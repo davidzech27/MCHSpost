c60a7cca8e19616b1847f974f53e846c7a6cbcb5
David Z <davidzech27@gmail.com>
2022-05-24 17:36:16 -0700

    set default open value made backdrop z 50

--- components/modal/Modal.jsx
+++ components/modal/Modal.jsx
@@ -3,7 +3,7 @@ import useModalContext from "/hooks/context/useModalContext"
 import Card from "/components/wrapper/Card"
 import SmallButton from "/components/input/SmallButton"
 
-const Modal = ({ open, children }) => {
+const Modal = ({ open = true, children }) => {
     const { setModal } = useModalContext()
 
     const close = () => setModal(null)
@@ -11,10 +11,10 @@ const Modal = ({ open, children }) => {
     if (!open) close()
 
     return (
-        <div onClick={close} className="h-screen w-screen fixed z-10 bg-background bg-opacity-85 flex justify-center items-center">
+        <div onClick={close} className="h-screen w-screen fixed z-50 bg-background bg-opacity-85 flex justify-center items-center">
             <Card onClick={(e) => e.stopPropagation()} className="w-[32rem] bg-surface1 relative">
                 <SmallButton onClick={close} className="absolute top-3 right-3 bg-surface3 hover:bg-hover">
-                    <svg className="h-6 w-6 m-auto" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}><path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" /></svg>
+                    <svg className="h-6 w-6 mx-auto" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}><path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" /></svg>
                 </SmallButton>
                 {children}
             </Card>
