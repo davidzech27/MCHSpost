78ab552842370899afb1eddc2f346889568ce4b2
David Z <davidzech27@gmail.com>
2022-05-24 18:03:39 -0700

    made UserDisplay component

---
+++ components/user/UserDisplay.jsx
@@ -0,0 +1,22 @@
+import Column from "/components/wrapper/Column"
+import Avatar from "/components/user/Avatar"
+
+const UserDisplay = ({ user, button}) => {
+    return (
+        <Column>
+            <Avatar user={user} className="h-32 w-32 mb-6 self-center" />
+        
+            <div className="text-center text-xl tracking-wider">{user.name}</div>
+            <div className="text-center text-xl text-subtext">{user.email}</div>
+            
+            <pre className="min-h-[48px] my-6 font-sans text-lg">{user.bio}</pre>
+            
+            <div className="flex justify-between">
+                {button}
+                <span className="text-subtext text-lg">Joined on {new Date(user.joinedOn).toDateString()}</span>
+            </div>
+        </Column>
+    )
+}
+ 
+export default UserDisplay
