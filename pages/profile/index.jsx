d5a0b1261abc8c222ccf416d80cc38427c5702a5
David Z <davidzech27@gmail.com>
2022-05-24 18:03:59 -0700

    made profile page

---
+++ pages/profile/index.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react"
+import useProfileContext from "/hooks/context/useProfileContext"
+
+import Card from "/components/wrapper/Card"
+import Column from "/components/wrapper/Column"
+import UserDisplay from "/components/user/UserDisplay"
+import SmallButton from "/components/input/SmallButton"
+import TextInput from "/components/input/TextInput"
+import TextArea from "/components/input/TextArea"
+
+const ProfilePage = () => {
+    const { profile, updateProfile } = useProfileContext()
+
+    const [editMode, setEditMode] = useState(false)
+
+    const [photo, setPhoto] = useState("")
+    const [name, setName] = useState("")
+    const [bio, setBio] = useState("")
+
+    const noInput = photo === "" && name === "" && bio === ""
+
+    const update = () => {
+        updateProfile({ photo, name, bio})
+
+        setEditMode(false)
+
+        setPhoto(""); setName(""); setBio("")
+    }
+
+    return (
+        <Card className="bg-surface1 px-16 py-8">
+            {profile ? 
+            <>{!editMode ?
+            <UserDisplay user={profile} button={<SmallButton onClick={() => setEditMode(true)} className="w-24 bg-yellow hover:bg-opacity-75 text-background">Edit</SmallButton>} /> :
+            <Column>
+                <TextArea value={photo} setValue={setPhoto} placeholder="Link to your new profile photo" className="h-32 w-32 self-center bg-surface2 focus:bg-surface3" />
+
+                <TextInput value={name} setValue={setName} placeholder="Your new display name" className="w-80 self-center bg-surface2 focus:bg-surface3" />
+                <div className="text-center text-xl text-subtext">{profile.email}</div>
+
+                <TextArea value={bio} setValue={setBio} placeholder="Your new bio" className="h-24 bg-surface2 focus:bg-surface3" />
+
+                <div className="flex justify-between">
+                    <span>
+                        <SmallButton onClick={update} disabled={noInput} className={`w-24 mr-1 bg-green text-background ${noInput ? "bg-opacity-40" : "hover:bg-opacity-75"}`}>Save</SmallButton>
+                        <SmallButton onClick={() => setEditMode(false)} className="w-24 bg-surface2 hover:bg-hover">Cancel</SmallButton>
+                    </span>
+                
+                    <span className="text-subtext text-lg">Joined on {new Date(profile.joinedOn).toDateString()}</span>
+                </div>
+            </Column>}</> :
+            "Loading..."}
+        </Card>
+    )
+}
+ 
+export default ProfilePage
