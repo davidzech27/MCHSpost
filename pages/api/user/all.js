c3f357ab1f88108793fde30b1cab8c405f2362be
David Z <davidzech27@gmail.com>
2022-05-26 21:54:47 -0700

    deleted unnecessary endpoint

--- pages/api/user/all.js
+++
@@ -1,9 +0,0 @@
-import User from "/models/User"
-import { withDB } from "/lib/db"
-
-const allUserHandler = async (req, res) => {
-    const allUsers = await User.find({}).select({ data: 0 }).lean() //consider unselecting user posts
-    res.status(200).json(allUsers)
-}
-
-export default withDB(allUserHandler)
