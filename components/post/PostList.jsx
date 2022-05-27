ddc6e2e798e9d9edcf56a458b9f5f09c311f24d0
David Z <davidzech27@gmail.com>
2022-05-27 09:37:16 -0700

    made PostList component

---
+++ components/post/PostList.jsx
@@ -0,0 +1,18 @@
+import Card from "/components/wrapper/Card"
+import Column from "/components/wrapper/Column"
+import PostCard from "/components/post/PostCard"
+
+const PostList = ({ posts }) => {
+    return (
+        <Card className="bg-background">
+            {posts ? 
+            <Column>
+                {posts.map((post) => <PostCard post={post} className="bg-surface2 hover:bg-surface3" key={post._id} />)}
+            </Column> :
+            "Loading..."
+            }
+        </Card>
+    )
+}
+ 
+export default PostList
