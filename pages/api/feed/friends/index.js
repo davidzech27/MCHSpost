d93199011a4205dfc64d040377aba49e6260c7dd
David Z <davidzech27@gmail.com>
2022-05-26 22:19:58 -0700

    made friend feed endpoint only take friend posts

--- pages/api/feed/friends/index.js
+++ pages/api/feed/friends/index.js
@@ -3,15 +3,18 @@ import User from "/models/User"
 import { getToken } from "next-auth/jwt"
 import { withDB } from "/lib/db"
 
-const friendsPostHandler = async (req, res) => {
+const friendFeedHandler = async (req, res) => {
     const { email } = await getToken({ req })
 
     const friends = await User.find({ email }).select({ friends: 1 }).lean()
     const friendsEmails = friends.map((friend) => friend.email)
-
-    const friendsPosts = await Post.find({ poster: { email: { $in: friendsEmails } } }).sort({ postedOn: -1 }).lean()//! $in not working
-
-    res.status(200).json(friendsPosts)
+    
+    if (friendsEmails[0]) {
+        const friendsPosts = await Post.find().where("postedBy.email").in([friendsEmails]).sort({ postedOn: -1 }).lean()
+        res.status(200).json(friendsPosts)
+    } else {
+        res.status(404).send("You have no friends!")
+    }
 }
 
 export default withDB(friendsPostHandler)
