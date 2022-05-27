d8b37b745b10d4705571512a970ddf6f325cad6e
David Z <davidzech27@gmail.com>
2022-05-27 10:26:37 -0700

    made FeedHeader handle loading state FeedHeader has same height as AboutPageLink

--- components/header/FeedHeader.jsx
+++ components/header/FeedHeader.jsx
@@ -5,17 +5,22 @@ import FeedTypeDropdown from "/components/header/FeedTypeDropdown"
 
 const FeedHeader = ({ profile, feedType, setFeedType }) => {
     return (
-        <Card className="bg-surface1 text-xl flex justify-between">
-            <Link href="/profile" passHref>
+        <Card className="h-21 bg-surface1 text-xl flex justify-between">
+            {profile ? 
+            <>
+                <Link href="/profile" passHref>
                 <div className="w-fit flex items-center cursor-pointer">
                     <Avatar user={profile} className="h-10 w-10" />
                     <span className="ml-6">{profile.name}</span>
                     <span className="ml-3 text-subtext">{profile.email}</span>
                 </div>
-            </Link>
-            <div className="flex items-center">
-                <FeedTypeDropdown feedType={feedType} setFeedType={setFeedType} />
-            </div>
+                </Link>
+                <div className="flex items-center">
+                    <FeedTypeDropdown feedType={feedType} setFeedType={setFeedType} />
+                </div>
+            </> :
+            "Loading..."
+            }
         </Card>
     )
 }
--- components/layout/AboutPageLink.jsx
+++ components/layout/AboutPageLink.jsx
@@ -2,7 +2,7 @@ import LinkCard from "/components/wrapper/LinkCard"
 
 const AboutPageLink = () => {
     return (
-        <LinkCard href="/about" className="bg-surface1 hover:bg-hover text-3xl text-center select-none px-0">
+        <LinkCard href="/about" className="h-21 bg-surface1 hover:bg-hover text-3xl text-center select-none px-0">
             <span className="text-green">MCHS</span>
             <span className="text-yellow">post</span>
         </LinkCard>
