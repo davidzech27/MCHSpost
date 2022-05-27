7a5a493cd257581f25cfa9211b13beb0b0eca7e6
David Z <davidzech27@gmail.com>
2022-05-27 09:38:25 -0700

    fixed css mistake

--- components/post/PostList.jsx
+++ components/post/PostList.jsx
@@ -4,7 +4,7 @@ import PostCard from "/components/post/PostCard"
 
 const PostList = ({ posts }) => {
     return (
-        <Card className="bg-background">
+        <Card className="bg-surface1">
             {posts ? 
             <Column>
                 {posts.map((post) => <PostCard post={post} className="bg-surface2 hover:bg-surface3" key={post._id} />)}
