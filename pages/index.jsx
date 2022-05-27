abf81b2b8dcfb077c10becbfdfa411f1ed7bee7c
David Z <davidzech27@gmail.com>
2022-05-26 22:13:31 -0700

    fixed tabs

--- pages/index.jsx
+++ pages/index.jsx
@@ -19,8 +19,8 @@ const LoadingScreen = () => {
 
 	return (
 		<div className="h-screen pt-72 text-center
-                    bg-gradient-to-r from-green to-yellow
-                    text-9xl text-text font-black">
+                        bg-gradient-to-r from-green to-yellow
+                        text-9xl text-text font-black">
             <span className="animate-pulse">MCHSpost</span>
         </div>
 	)
