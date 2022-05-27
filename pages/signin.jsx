242168bea0c867d3fb5c37f796e81ae67058db84
David Z <davidzech27@gmail.com>
2022-05-27 09:06:22 -0700

    made button text dark color

--- pages/signin.jsx
+++ pages/signin.jsx
@@ -3,7 +3,7 @@ import { signIn } from "next-auth/react"
 const SignInPage = () => {
 	return (
 		<div className="h-screen w-screen bg-background flex justify-center items-center">
-			<button onClick={() => signIn("google", { callbackUrl: "/home" })} className="h-60 w-[60rem] bg-gradient-to-tr from-green to-yellow hover:opacity-85 rounded-full text-6xl tracking-wider transition duration-100">
+			<button onClick={() => signIn("google", { callbackUrl: "/home" })} className="h-60 w-[60rem] bg-gradient-to-tr from-green to-yellow hover:opacity-85 rounded-full text-6xl text-surface2 tracking-wider transition duration-100">
                 Sign in with your
                 <br />
                 @srcschools.org email acount
