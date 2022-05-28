3d45f1e7c40f842318d873b603a20884ff8eefd4
David Z <davidzech27@gmail.com>
2022-05-28 09:54:33 -0700

    removed unnecessary spaces

--- pages/api/friend/request/[email].js
+++ pages/api/friend/request/[email].js
@@ -9,7 +9,7 @@ const friendRequestHandler = async (req, res) => {
     if (selfEmail === requestEmail) {
         res.status(400).send("You can't be friends with yourself!")
     } else {
-        await User.updateOne({ email: requestEmail }, { $addToSet: { "data.friendReqs": selfEmail } })  
+        await User.updateOne({ email: requestEmail }, { $addToSet: { "data.friendReqs": selfEmail } })
 
         res.status(200).end()
     }
