11ea3d45d9f79dfc691bc25ad7f22a5bcb45de3f
David Z <davidzech27@gmail.com>
2022-05-28 10:20:54 -0700

    added endpoint for accepting friend requests

---
+++ pages/api/friend/accept/[email].js
@@ -0,0 +1,31 @@
+import User from "/models/User"
+import { getToken } from "next-auth/jwt"
+import { withDB } from "/lib/db"
+
+const acceptFriendRequestHandler = async (req, res) => {
+    const { email: selfEmail } = await getToken({ req })
+    const { email: friendEmail } = req.query
+
+    const [self, friend] = await Promise.all([
+        User.findOne({ selfEmail }).select({ friends: 1, "data.friendReqs": 1 }),
+        User.findOne({ friendEmail }).select({ friends: 1, "data.friendReqs": 1 })
+    ])
+
+    if (self.data.friendReqs.includes(friendEmail)) {
+        delete self.data.friendReqs[self.data.friendReqs.indexOf(friendEmail)]
+
+        self.friends.push(friendEmail); friend.friends.push(selfEmail)
+
+        await Promise.all([self.save(), friend.save()])
+
+        res.status(200).end()
+    } else {
+        res.status(400).send("You don't have a friend request from user!")
+    }
+
+
+
+    
+}
+
+export default withDB(acceptFriendRequestHandler)
