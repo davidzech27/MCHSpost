fc5ca53fb10c30a51856d17b727f148c77bd90e6
David Z <davidzech27@gmail.com>
2022-05-28 00:41:15 -0700

    improved placeholder post page

--- pages/post/index.jsx
+++ pages/post/index.jsx
@@ -1,13 +1,21 @@
-import { useState, useEffect } from "react"
 import useClientRouteParam from "/hooks/util/useClientRouteParam"
+import usePost from "/hooks/api/usePost"
+
+import Card from "/components/wrapper/Card"
+import PostCard from "/components/post/PostCard"
 
 const PostPage = () => {
     const postId = useClientRouteParam()
 
+    const post = usePost(postId)
+
 	return (
-		<>
-            <div>post id: {postId}</div>
-		</>
+		<Card className="bg-surface1">
+            {post ?
+            <PostCard post={post} className="bg-surface2 hover:bg-surface3" /> :
+            "Loading..."
+            }
+        </Card>
 	)
 }
 
