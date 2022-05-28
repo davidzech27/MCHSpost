ed9be5ba7508ce00b8512b682561452e1fccfe9b
David Z <davidzech27@gmail.com>
2022-05-28 00:24:29 -0700

    fixed post sorting

--- pages/api/user/[email]/posts.js
+++ pages/api/user/[email]/posts.js
@@ -4,7 +4,10 @@ import { withDB } from "/lib/db"
 
 const userPostHandler = async (req, res) => {
     const { email } = req.query
-    const { posts } = await User.findOne({ email }).populate("posts").select({ posts: 1 }).sort({ postedOn: -1 }).exec()
+
+    const { posts } = await User.findOne({ email }).populate("posts").select({ posts: 1 }).exec()
+    
+    posts.sort((post1, post2) => post2.postedOn.getTime() - post1.postedOn.getTime())
 
     res.status(200).json(posts)
 }
