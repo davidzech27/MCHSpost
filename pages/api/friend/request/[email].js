9a8ec6cdd9bcef05adbaf04c3fc17ae2e383a091
David Z <davidzech27@gmail.com>
2022-05-28 09:50:10 -0700

    prevent user from sending friendReq to themself

--- pages/api/friend/request/[email].js
+++ pages/api/friend/request/[email].js
@@ -6,9 +6,13 @@ const friendRequestHandler = async (req, res) => {
     const { email: selfEmail } = await getToken({ req })
     const { email: requestEmail } = req.query
 
-    await User.updateOne({ email: requestEmail }, { $addToSet: { "data.friendReqs": selfEmail } })  
+    if (selfEmail === requestEmail) {
+        res.status(400).send("You can't be friends with yourself!")
+    } else {
+        await User.updateOne({ email: requestEmail }, { $addToSet: { "data.friendReqs": selfEmail } })  
 
-    res.status(200).end()
+        res.status(200).end()
+    }
 }
 
 export default withDB(friendRequestHandler)
