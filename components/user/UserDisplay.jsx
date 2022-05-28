0cce65b3010fb297642759076d8b4d26add8cc8b
David Z <davidzech27@gmail.com>
2022-05-28 00:06:19 -0700

    now handles loading state from within component

--- components/user/UserDisplay.jsx
+++ components/user/UserDisplay.jsx
@@ -4,19 +4,24 @@ import Avatar from "/components/user/Avatar"
 const UserDisplay = ({ user, button }) => {
     return (
         <Column className="mx-10 my-2">
-            <Avatar user={user} className="h-32 w-32 self-center" />
-        
-            <div className="mt-2.5 text-center text-2xl tracking-wider">{user.name}</div>
-            <div className="mb-2.5 text-center text-2xl text-subtext">{user.email}</div>
+            {user ?
+            <>
+                <Avatar user={user} className="h-32 w-32 self-center" />
             
-            <div className="min-h-[108px]">
-                <pre className="px-4 py-3 bg-surface2 rounded-xl font-sans text-xl">{user.bio}</pre>
-            </div>
-            
-            <div className="flex justify-between">
-                {button ?? <div />}
-                <span className="text-subtext text-lg">Joined on {new Date(user.joinedOn).toDateString()}</span>
-            </div>
+                <div className="mt-2.5 text-center text-2xl tracking-wider">{user.name}</div>
+                <div className="mb-2.5 text-center text-2xl text-subtext">{user.email}</div>
+                
+                <div className="min-h-[108px]">
+                    <pre className="px-4 py-3 bg-surface2 rounded-xl font-sans text-xl">{user.bio}</pre>
+                </div>
+                
+                <div className="flex justify-between">
+                    {button ?? <div />}
+                    <span className="text-subtext text-lg">Joined on {new Date(user.joinedOn).toDateString()}</span>
+                </div>
+            </> :
+            "Loading..."
+            }
         </Column>
     )
 }
