51917302eb0e8db4e19550eb93c0daa26dc2d06a
David Z <davidzech27@gmail.com>
2022-05-28 00:35:23 -0700

    now uses PostList

--- pages/user/index.jsx
+++ pages/user/index.jsx
@@ -2,20 +2,21 @@ import useClientRouteParam from "/hooks/util/useClientRouteParam"
 import useUser from "/hooks/api/useUser"
 
 import Card from "/components/wrapper/Card"
+import Column from "/components/wrapper/Column"
 import UserDisplay from "/components/user/UserDisplay"
+import PostList from "/components/post/PostList"
 
 const UserPage = () => {
     const email = useClientRouteParam()
 
     const user = useUser(email)
-    
+
     return (
-        <Card className="bg-surface1">
-            {user ?
-            <UserDisplay user={user} button={null} /> :
-            "Loading..."
-            }
-        </Card>
+        <Column>
+            <UserDisplay user={user} button={null} />
+            
+            <PostList posts={user?.posts} />
+        </Column>        
     )
 }
  
