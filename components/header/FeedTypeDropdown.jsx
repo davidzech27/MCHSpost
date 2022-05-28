b1d2f8ba26a923b3d70fa358d2c190f1e5f91b9e
David Z <davidzech27@gmail.com>
2022-05-28 10:09:46 -0700

    moved svg className to front of html properties

--- components/header/FeedTypeDropdown.jsx
+++ components/header/FeedTypeDropdown.jsx
@@ -8,7 +8,7 @@ const FeedTypeDropdown = ({ feedType, setFeedType }) => {
         <div className="h-10 w-36 relative">
             <div onMouseLeave={() => setOpen(false)} className="h-[156px] absolute rounded-lg">
                 <button onMouseEnter={() => {setOpen(true)}} className={`h-10 w-36 hover:bg-hover rounded-lg transition duration-100 cursor-default ${open ? "bg-surface3" : "bg-surface2"}`}>
-                    {feedType === "public" ? "Public feed" : "Friend feed"}<svg xmlns="http://www.w3.org/2000/svg" className="h-[22px] w-[22px] inline relative left-1 top-[-1px]" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}><path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" /></svg>
+                    {feedType === "public" ? "Public feed" : "Friend feed"}<svg className="h-[22px] w-[22px] inline relative left-1 top-[-1px]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}><path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" /></svg>
                 </button>
 
                 {open && <div className="h-28 w-36 p-4 absolute bottom-0 bg-surface1 hover:bg-surface2 rounded-lg shadow-lg shadow-background flex flex-col justify-around transition duration-100">
