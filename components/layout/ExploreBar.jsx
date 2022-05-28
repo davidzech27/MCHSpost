b3b48de2029a47795bb1971f0784eb16ec5b2974
David Z <davidzech27@gmail.com>
2022-05-28 00:06:02 -0700

    made top card height match header height

--- components/layout/ExploreBar.jsx
+++ components/layout/ExploreBar.jsx
@@ -4,7 +4,7 @@ import Card from "/components/wrapper/Card"
 const ExploreBar = () => {
     return (
         <Column>
-            <Card className="h-64 bg-surface1">
+            <Card className="h-21 bg-surface1">
 
             </Card>
             <Card className="h-96 bg-surface1">
