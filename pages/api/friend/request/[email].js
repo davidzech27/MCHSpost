bfbc3d79733941e2310821e50a95c285480e35fc
David Z <davidzech27@gmail.com>
2022-05-28 10:48:56 -0700

    friend endpoints require post method

--- pages/api/friend/request/[email].js
+++ pages/api/friend/request/[email].js
@@ -3,15 +3,19 @@ import { getToken } from "next-auth/jwt"
 import { withDB } from "/lib/db"
 
 const friendRequestHandler = async (req, res) => {
-    const { email: selfEmail } = await getToken({ req })
-    const { email: requestEmail } = req.query
+    if (req.method === "POST") {
+        const { email: selfEmail } = await getToken({ req })
+        const { email: requestEmail } = req.query
 
-    if (selfEmail !== requestEmail) {
-        await User.updateOne({ email: requestEmail }, { $addToSet: { "data.friendReqs": selfEmail } })
+        if (selfEmail !== requestEmail) {
+            await User.updateOne({ email: requestEmail }, { $addToSet: { "data.friendReqs": selfEmail } })
 
-        res.status(200).end()
+            res.status(200).end()
+        } else {
+            res.status(400).send("You can't be friends with yourself!")
+        }
     } else {
-        res.status(400).send("You can't be friends with yourself!")
+        res.status(405).end()
     }
 }
 
