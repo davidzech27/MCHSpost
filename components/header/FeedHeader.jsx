b1bd181c675bcd557d5f679ff65c7ef8f5a19e8a
David Z <davidzech27@gmail.com>
2022-05-29 11:58:53 -0700

    made styles not affect loading state

--- components/header/FeedHeader.jsx
+++ components/header/FeedHeader.jsx
@@ -5,20 +5,20 @@ import FeedTypeDropdown from "/components/header/FeedTypeDropdown"
 
 const FeedHeader = ({ profile, feedType, setFeedType }) => {
     return (
-        <Card className="h-21 bg-surface1 text-xl flex justify-between">
+        <Card className="h-21 bg-surface1">
             {profile ? 
-            <>
+            <div className="text-xl flex justify-between">
                 <Link href="/profile" passHref>
-                <div className="w-fit flex items-center cursor-pointer">
-                    <Avatar user={profile} className="h-10 w-10" />
-                    <span className="ml-6">{profile.name}</span>
-                    <span className="ml-3 text-subtext">{profile.email}</span>
-                </div>
+                    <div className="w-fit flex items-center cursor-pointer">
+                        <Avatar user={profile} className="h-10 w-10" />
+                        <span className="ml-6">{profile.name}</span>
+                        <span className="ml-3 text-subtext">{profile.email}</span>
+                    </div>
                 </Link>
                 <div className="flex items-center">
                     <FeedTypeDropdown feedType={feedType} setFeedType={setFeedType} />
                 </div>
-            </> :
+            </div> :
             "Loading..."
             }
         </Card>
