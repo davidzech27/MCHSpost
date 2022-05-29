f3d970123bdda6dfe7b685fdf1da7414b0092812
David Z <davidzech27@gmail.com>
2022-05-28 23:56:15 -0700

    made usePost use initial data from feed

--- hooks/api/usePost.js
+++ hooks/api/usePost.js
@@ -1,7 +1,16 @@
-import { useQuery } from "react-query"
+import { useQuery, useQueryClient } from "react-query"
+import useStickyState from "/hooks/util/useStickyState"
 
 const usePost = (postId) => {
-    const { data: post } = useQuery(`/post/${postId}`)
+    const queryClient = useQueryClient()
+
+    const [feedType, _] = useStickyState("feedType", "public")
+
+    const { data: post } = useQuery(`/post/${postId}`, {
+        initialData: () => {
+            return queryClient.getQueryData(`/feed/${feedType}`)?.find((post) => post._id === postId)
+        }
+    })
     
     return post
 }
