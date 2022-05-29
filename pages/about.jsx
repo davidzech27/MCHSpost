5ee5bf25f8dc34f865401fe47c8b4a2dc23a383f
David Z <davidzech27@gmail.com>
2022-05-29 01:15:05 -0700

    added contact to about page

--- pages/about.jsx
+++ pages/about.jsx
@@ -2,8 +2,10 @@ import Card from "/components/wrapper/Card"
 
 const AboutPage = () => {
     return (
-        <Card className="bg-surface1">
+        <Card className="bg-surface1 text-lg">
             About page not yet made.
+            <br /><br />
+            Contact me <a href="https://mail.google.com/mail/u/0/?to=david_z2463@srcschools.org&su=MCHSpost&tf=cm" target="_blank" className="underline">here</a>
         </Card>
     )
 }
