774fcd8d68632c50abec8ae1b955512eaea5ab20
David Z <davidzech27@gmail.com>
2022-05-29 00:05:20 -0700

    added add friend button to user page

--- pages/user/index.jsx
+++ pages/user/index.jsx
@@ -1,19 +1,23 @@
 import useClientRouteParam from "/hooks/util/useClientRouteParam"
 import useUser from "/hooks/api/useUser"
+import useFriendRequest from "/hooks/api/useFriendRequest"
 
-import Card from "/components/wrapper/Card"
 import Column from "/components/wrapper/Column"
 import UserDisplay from "/components/user/UserDisplay"
 import PostList from "/components/post/PostList"
+import SmallButton from "/components/input/SmallButton"
 
 const UserPage = () => {
     const email = useClientRouteParam()
 
     const user = useUser(email)
+    const { sendFriendRequest } = useFriendRequest()
 
     return (
         <Column>
-            <UserDisplay user={user} button={null} />
+            <UserDisplay user={user} button={
+                <SmallButton onClick={() => sendFriendRequest(email)} className="px-3 bg-green hover:bg-opacity-75 text-background">Add friend</SmallButton>
+            } />
             
             <PostList posts={user?.posts} />
         </Column>        
