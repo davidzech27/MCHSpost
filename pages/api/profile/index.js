b9f9ec138661d6d8c3ea6bc47c122b1cf0f0397d
David Z <davidzech27@gmail.com>
2022-05-29 11:14:30 -0700

    profile endpoint no longer returns posts

--- pages/api/profile/index.js
+++ pages/api/profile/index.js
@@ -6,7 +6,7 @@ const profileHandler = async (req, res) => {
     const email = (await getToken({ req }))?.email
 
     if (email) {
-        const profile = await User.findOne({ email }).lean()
+        const profile = await User.findOne({ email }).select({ posts: 0, friends: 0, data: 0 }).lean()
         res.status(200).json(profile)
     } else {
         res.status(204).end()
