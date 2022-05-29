0df1696ff988ef02ebd05cb62fa241ec19b04549
David Z <davidzech27@gmail.com>
2022-05-29 00:55:11 -0700

    made placeholder about page

---
+++ pages/about.jsx
@@ -0,0 +1,11 @@
+import Card from "/components/wrapper/Card"
+
+const AboutPage = () => {
+    return (
+        <Card className="bg-surface1">
+            About page not yet made.
+        </Card>
+    )
+}
+
+export default AboutPage
