0806010dd4188a0654a7f5d7a7fd2d8fea3263d6
David Z <davidzech27@gmail.com>
2022-06-02 23:50:50 -0700

    made PostDisplay component

---
+++ components/post/PostDisplay.jsx
@@ -0,0 +1,23 @@
+import Card from "/components/wrapper/Card"
+import UserInfo from "/components/user/UserInfo"
+
+const PostDisplay = ({ post }) => {
+    return (
+        <Card className="bg-surface1">
+            {post ?
+            <>
+                <UserInfo user={post.postedBy} />
+                <br />
+                <pre className="text-lg font-sans">{post.text}</pre>
+                <br />
+                <div className="pr-6 text-subtext flex justify-end">
+                    <span>{new Date(post.postedOn).toDateString()}</span>
+                </div>
+            </> :
+            "Loading..."
+            }
+        </Card>
+    )
+}
+ 
+export default PostDisplay
