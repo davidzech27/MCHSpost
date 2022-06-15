f1ddbddbb13fddb9ef0f06e60333b6a8c239c077
David Z <davidzech27@gmail.com>
2022-06-15 16:27:38 -0700

    user posts endpoint handles when no user

--- pages/api/user/[email]/posts.js
+++ pages/api/user/[email]/posts.js
@@ -5,12 +5,18 @@ import { withDB } from "/lib/db"
 const userPostHandler = async (req, res) => {
     const { email } = req.query
 
-    let { posts } = await User.findOne({ email }).populate("posts").select({ posts: 1 }).exec()
-    
-    posts = posts.map(({ comments, ...post }) => ({ ...post._doc, commentCount: comments.length }))
-    posts.sort((post1, post2) => post2.postedOn.getTime() - post1.postedOn.getTime())
+    const user = await User.findOne({ email }).populate("posts").select({ posts: 1 }).exec()
 
-    res.status(200).json(posts)
+    if (user) {
+        let posts = user.toObject().posts
+
+        posts = posts.map(({ comments, ...post }) => ({ ...post, commentCount: comments.length }))
+        posts.sort((post1, post2) => post2.postedOn.getTime() - post1.postedOn.getTime())
+
+        res.status(200).json(posts)
+    } else {
+        res.status(404).end()
+    }
 }
 
 export default withDB(userPostHandler)
