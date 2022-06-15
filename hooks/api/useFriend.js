d9c2dea336a4ad5e9fe8e3fcbd91b52d63fccb48
David Z <davidzech27@gmail.com>
2022-06-15 16:49:24 -0700

    now fetches user's friends and friend requests

--- hooks/api/useFriend.js
+++ hooks/api/useFriend.js
@@ -1,30 +1,37 @@
-import { useMutation, useQueryClient } from "react-query"
+import { useQuery, useMutation, useQueryClient } from "react-query"
 import api from "/lib/api"
 
 const useFriend = () => {
     const queryClient = useQueryClient()
 
+    const { data: friendRequests } = useQuery("/friend/requests")
+
+    const { data: friends } = useQuery(`/user/${queryClient.getQueryData("/profile")?.email}/friends`, {
+        enabled: Boolean(queryClient.getQueryData("/profile"))
+    })
+
     const { mutate: sendFriendRequest } = useMutation(async (email) => {
-        await api.post(`friend/request/${email}`)
+        await api.post(`/friend/request/${email}`)
     })
 
     const { mutate: acceptFriendRequest } = useMutation(async (email) => {
-        await api.post(`friend/accept/${email}`)
+        await api.post(`/friend/accept/${email}`)
     }, {
-        onSuccess: async () => {
-            await queryClient.prefetchQuery("/profile")
+        onSuccess: () => {
+            queryClient.invalidateQueries(`/user/${queryClient.getQueryData("/profile").email}/friends`)
+            queryClient.invalidateQueries("/friend/requests")
         }
     })
 
     const { mutate: unfriendUser } = useMutation(async (email) => {
-        await api.post(`friend/unfriend/${email}`)
+        await api.post(`/friend/unfriend/${email}`)
     }, {
-        onSuccess: async () => {
-            await queryClient.prefetchQuery("/profile")
+        onSuccess: () => {
+            queryClient.invalidateQueries(`/user/${queryClient.getQueryData("/profile").email}/friends`)
         }
     })
 
-    return { sendFriendRequest, acceptFriendRequest, unfriendUser }
+    return { friendRequests, friends, sendFriendRequest, acceptFriendRequest, unfriendUser }
 }
 
 export default useFriend
