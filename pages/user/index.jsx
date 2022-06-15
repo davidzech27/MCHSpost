7067eb18ad860e8a28dadcc00bdd7c17303107f2
David Z <davidzech27@gmail.com>
2022-06-15 16:47:00 -0700

    button changes to accept request and add friend

--- pages/user/index.jsx
+++ pages/user/index.jsx
@@ -11,13 +11,20 @@ const UserPage = () => {
     const email = useClientRouteParam()
 
     const user = useUser(email)
-    const { sendFriendRequest } = useFriend()
+    const { friendRequests, friends, sendFriendRequest, acceptFriendRequest, unfriendUser } = useFriend()
+
+    let button = null
+    if (friends && friends.map((friend) => friend.email).includes(email)) {
+        button = <SmallButton onClick={() => unfriendUser(email)} className="px-2.5 bg-yellow hover:bg-opacity-75 text-background">Remove friend</SmallButton>
+    } else if (friendRequests && friendRequests.map((friendRequest) => friendRequest.email).includes(email)) {
+        button = <SmallButton onClick={() => acceptFriendRequest(email)} className="px-2.5 bg-green hover:bg-opacity-75 text-background">Accept friend request</SmallButton>
+    } else if (friends && friendRequests) {
+        button = <SmallButton onClick={() => sendFriendRequest(email)} className="px-2.5 bg-green hover:bg-opacity-75 text-background">Add friend</SmallButton>
+    }
 
     return (
         <Column>
-            <UserDisplay user={user} button={
-                <SmallButton onClick={() => sendFriendRequest(email)} className="px-2.5 bg-green hover:bg-opacity-75 text-background">Add friend</SmallButton>
-            } />
+            <UserDisplay user={user} button={button} />
             
             <PostList posts={user?.posts} />
         </Column>        
