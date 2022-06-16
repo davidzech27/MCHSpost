e0cb34d005e955aaeaac7433c77997d22a18d6ae
David Z <davidzech27@gmail.com>
2022-06-16 00:55:56 -0700

    friends button doesnt show if unauthenticated

--- components/layout/NavButtons.jsx
+++ components/layout/NavButtons.jsx
@@ -17,7 +17,7 @@ const NavButtons = () => {
             <Column>
                 <LinkButton href="/home" className="bg-surface2 hover:bg-hover">Home</LinkButton>
                 {!unauthenticated && <LinkButton href="/profile" className="bg-surface2 hover:bg-hover">Profile</LinkButton>}
-                <LinkButton href="/friends" className="bg-green hover:opacity-75 text-background">Friends</LinkButton>
+                {!unauthenticated && <LinkButton href="/friends" className="bg-green hover:opacity-75 text-background">Friends</LinkButton>}
                 {!unauthenticated && <Button onClick={() => openModal(<UploadModal />)} className="bg-yellow hover:opacity-75 text-background">Post</Button>}
                 {unauthenticated && <LinkButton href="/signin" className="bg-yellow hover:opacity-75 text-background">Sign in</LinkButton>}
             </Column>
