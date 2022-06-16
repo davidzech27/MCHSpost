002bc552af276eb15b4770edeaf3ff555397018e
David Z <davidzech27@gmail.com>
2022-06-16 01:45:33 -0700

    prevent client side error for not signed in users

--- pages/user/index.jsx
+++ pages/user/index.jsx
@@ -17,12 +17,14 @@ const UserPage = () => {
 
     let button = null
     const requestSent = useRef(false)
-    if (friends && friends.map((friend) => friend.email).includes(email)) {
-        button = <SmallButton onClick={() => unfriendUser(email)} className="px-2.5 bg-yellow hover:bg-opacity-75 text-background">Remove friend</SmallButton>
-    } else if (friendRequests && friendRequests.map((friendRequest) => friendRequest.email).includes(email)) {
-        button = <SmallButton onClick={() => acceptFriendRequest(email)} className="px-2.5 bg-green hover:bg-opacity-75 text-background">Accept friend request</SmallButton>
-    } else if (friends && friendRequests) {
-        button = <SmallButton onClick={() => { sendFriendRequest(email); requestSent.current = true }} disabled={requestSent.current} className={`px-2.5 bg-green text-background ${requestSent.current ? "bg-opacity-40" : "hover:bg-opacity-75"}`}>Add friend</SmallButton>
+    if (friends && friendRequests) {
+        if (friends && friends.map((friend) => friend.email).includes(email)) {
+            button = <SmallButton onClick={() => unfriendUser(email)} className="px-2.5 bg-yellow hover:bg-opacity-75 text-background">Remove friend</SmallButton>
+        } else if (friendRequests && friendRequests.map((friendRequest) => friendRequest.email).includes(email)) {
+            button = <SmallButton onClick={() => acceptFriendRequest(email)} className="px-2.5 bg-green hover:bg-opacity-75 text-background">Accept friend request</SmallButton>
+        } else {
+            button = <SmallButton onClick={() => { sendFriendRequest(email); requestSent.current = true }} disabled={requestSent.current} className={`px-2.5 bg-green text-background ${requestSent.current ? "bg-opacity-40" : "hover:bg-opacity-75"}`}>Add friend</SmallButton>
+        }
     }
 
     return (
