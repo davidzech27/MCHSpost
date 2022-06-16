eb112eb42595da617d1e94bdf30970bd1bda16e3
David Z <davidzech27@gmail.com>
2022-06-15 19:46:59 -0700

    added rel="noreferrer" to target="_blank" links

--- components/layout/ExploreBar.jsx
+++ components/layout/ExploreBar.jsx
@@ -27,7 +27,7 @@ const ExploreBar = () => {
             <Card className="h-96 bg-surface1 text-subtext text-2xl text-center">
                 Advertise your club here!
                 <br />
-                Enquire <a href="https://mail.google.com/mail/u/0/?to=david_z2463@srcschools.org&su=MCHSpost+club+advertising&tf=cm" target="_blank" className="underline">here</a>
+                Enquire <a href="https://mail.google.com/mail/u/0/?to=david_z2463@srcschools.org&su=MCHSpost+club+advertising&tf=cm" target="_blank" rel="noreferrer" className="underline">here</a>
             </Card>
         </Column>
     )
--- pages/about.jsx
+++ pages/about.jsx
@@ -5,7 +5,7 @@ const AboutPage = () => {
         <Card className="bg-surface1 text-lg">
             About page not yet made.
             <br /><br />
-            Contact me <a href="https://mail.google.com/mail/u/0/?to=david_z2463@srcschools.org&su=MCHSpost&tf=cm" target="_blank" className="underline">here</a>
+            Contact me <a href="https://mail.google.com/mail/u/0/?to=david_z2463@srcschools.org&su=MCHSpost&tf=cm" target="_blank" rel="noreferrer" className="underline">here</a>
         </Card>
     )
 }
