bad07c6877143481100872489baa09f9a27d3d59
David Z <davidzech27@gmail.com>
2022-06-15 19:30:38 -0700

    made add user button disabled if already pressed

--- pages/user/index.jsx
+++ pages/user/index.jsx
@@ -1,3 +1,5 @@
+import { useRef } from "react"
+
 import useClientRouteParam from "/hooks/util/useClientRouteParam"
 import useUser from "/hooks/api/useUser"
 import useFriend from "/hooks/api/useFriend"
@@ -14,12 +16,13 @@ const UserPage = () => {
     const { friendRequests, friends, sendFriendRequest, acceptFriendRequest, unfriendUser } = useFriend()
 
     let button = null
+    const requestSent = useRef(false)
     if (friends && friends.map((friend) => friend.email).includes(email)) {
         button = <SmallButton onClick={() => unfriendUser(email)} className="px-2.5 bg-yellow hover:bg-opacity-75 text-background">Remove friend</SmallButton>
     } else if (friendRequests && friendRequests.map((friendRequest) => friendRequest.email).includes(email)) {
         button = <SmallButton onClick={() => acceptFriendRequest(email)} className="px-2.5 bg-green hover:bg-opacity-75 text-background">Accept friend request</SmallButton>
     } else if (friends && friendRequests) {
-        button = <SmallButton onClick={() => sendFriendRequest(email)} className="px-2.5 bg-green hover:bg-opacity-75 text-background">Add friend</SmallButton>
+        button = <SmallButton onClick={() => { sendFriendRequest(email); requestSent.current = true }} disabled={requestSent.current} className={`px-2.5 bg-green text-background ${requestSent.current ? "bg-opacity-40" : "hover:bg-opacity-75"}`}>Add friend</SmallButton>
     }
 
     return (
